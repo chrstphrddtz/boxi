594096c62bfd06bb0797fc79776007b70997b045
christoph.raddatz <hello@chrstphrddtz.com>
2023-04-18 16:22:23 +0200

    add first layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,9 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <div className={styles.main}>
        <h1>Welcome to BOXI ...</h1>
-      </main>
+      </div>
     </>
   )
 }
---
+++ src/pages/results/index.tsx
@@ -0,0 +1,12 @@
+import styles from '@/styles/Home.module.css'
+
+
+export default function ResultView() {
+
+  return(
+    <>
+      <h1 className={styles.main}>Results</h1>
+    </>
+  )
+
+}
