1ccbcc9b4eddba94d5dfa94a89d75cebe50e3fe9
christoph.raddatz <hello@chrstphrddtz.com>
2023-04-18 15:32:59 +0200

    add first basic layout to navbar

---
+++ db/models/User.ts
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+  
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import { Figtree } from 'next/font/google'
 import type { AppProps } from 'next/app'
+
+const figtree = Figtree({ 
+  weight: ['400', '600', '800'],
+  style: ['italic', 'normal'],
+  subsets: ['latin'] 
+})
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
