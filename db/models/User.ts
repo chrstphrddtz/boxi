4451ccf2018c501233ede1426475eb2908d3b390
christoph.raddatz <hello@chrstphrddtz.com>
2023-04-18 17:08:10 +0200

    add User Schema

--- db/models/User.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const { Schema } = mongoose
-
-const userSchema = new Schema({
-  
-})
---
+++ db/models/User.tsx
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+  // UNCOMMENT WHEN IMPLEMENT USER AUTH
+  // email: {
+  //   type: String,
+  //   required: true,
+  //   unique: [true, "Account already exists"],
+  //   validate: [validator.isEmail, "Please enter a valid email"]
+  // },
+  // password: {
+  //   type: String,
+  //   required: [true, "Please enter your email"],
+  //   minLength: [8, "Your password must be at least 6 characters long"],
+  //   select: false, //dont send back password after request
+  // },
+  // role: {
+  //   type: String,
+  //   default: "user",
+  //   enum: {
+  //     values: [
+  //       "user",
+  //       "admin"
+  //     ],
+  //   }
+  // },
+  // createdAt: {
+  //   type: Date,
+  //   default: Date.now
+  // },
+  name: { type: String, required: true },
+  location: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: false }
+})
