7ef55bc68d760bc254f5fa3915b59dcaa93bd21b
christoph.raddatz <hello@chrstphrddtz.com>
2023-04-21 16:56:32 +0200

    add comments

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -10,11 +10,14 @@ const SearchContainer = styled.article`
 
 
 export default function Search() {
-  const [user, setUser] = useState("");
+  const [query, setQuery] = useState('');
   const router = useRouter();
   const { isReady } = router;
   const { data, isLoading, error } = useSWR("/api/users", { fallbackData: [] });
-  console.log("From Search: ", data);
+  
+  console.log("Data from Search: ", data);
+  console.log("Query from Search: ", query);
+  
   
 
   if (!isReady || isLoading || error) return <h2>Loading...</h2>;
@@ -24,7 +27,17 @@ export default function Search() {
   // Input for Price (Slider)
   // Search Button 
   // Should redirect the user to /results 
+
+  // ?? next.js search using query string
   // in /results the user will see only the filtered items
+
+  function searchFilter(userData: any) {
+    const filterByLocation = userData.filter((element: any) => {
+      element.location.toLowerCase().includes(query)
+    })
+
+  }
+
 
   return (
     <SearchContainer>
