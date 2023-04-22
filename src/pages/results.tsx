8bd5d8f7378bb22f064fce80ade9433548d00c95
christoph.raddatz <hello@chrstphrddtz.com>
2023-04-22 12:38:10 +0200

    delete comments

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -7,7 +7,6 @@ import UserList from "../../components/UserList";
 import OfferView from "../../components/OfferView";
 
 const MainContainer = styled.div`
-  /* gap: 2rem; */
   height: 95vh;
   display: grid;
   gap: 0.5rem;
@@ -20,11 +19,8 @@ const MainContainer = styled.div`
 `;
 
 const ListContainer = styled.div`
-  /* border-radius: 0.7rem; */
-  /* margin-top: -1rem; */
   margin: -0.9rem auto;
   overflow-x: hidden;
-  /* height: 100vh; */
   ::-webkit-scrollbar {
     display: none;
   };
@@ -32,12 +28,8 @@ const ListContainer = styled.div`
 
 const OfferContainer = styled.div`
   background-color: #E2AC55;
-  /* border: 2px solid black; */
-  /* border-radius: 0.9rem; */
   margin-bottom: 1rem;
-  /* margin-top: 1.5rem; */
   overflow-x: hidden;
-  /* height: 100vh; */
   ::-webkit-scrollbar {
     display: none;
   };
