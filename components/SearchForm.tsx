2867aaeb1a67ab352313193676739c1940e12c02
christoph.raddatz <hello@chrstphrddtz.com>
2023-04-24 16:51:00 +0200

    Add first Search functionality

---
+++ components/SearchForm.tsx
@@ -0,0 +1,64 @@
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import { StyledButton } from "./StyledButton";
+
+const FormContainer = styled.form`
+  display: grid;
+  gap: 0.5rem;
+  width: 20rem;
+`;
+
+const Label = styled.label`
+  font-weight: bold;
+`;
+
+const Input = styled.input`
+  padding: 0.5rem;
+  font-size: inherit;
+  border: 1px solid black;
+  background-color: #f3e8d7;
+  border-radius: 0.3rem;
+  font-size: 1rem;
+`;
+
+export default function SearchForm({ onSubmit, formName, onClick }: any) {
+  const router = useRouter();
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    onSubmit(event);
+  }
+
+  return (
+    <FormContainer aria-labelledby={formName} onSubmit={handleSubmit}>
+      <Label htmlFor="location">Location</Label>
+      <Input
+        required
+        id="location"
+        name="location"
+        type="text"
+        defaultValue="Berlin"
+      />
+
+      {/* <Label htmlFor="date">Availability</Label>
+      <Input
+        required
+        id="date"
+        name="date"
+        type="date"
+        // defaultValue={}
+      /> */}
+
+      <Label htmlFor="price">Price</Label>
+      <Input
+        id="price"
+        name="price"
+        type="range"
+        // defaultValue={}
+      />
+
+      <StyledButton type="submit" onClick={onClick}>
+        Search
+      </StyledButton>
+    </FormContainer>
+  );
+}
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,53 +1,50 @@
 import { useRouter } from "next/router";
-import useSWR from "swr";
 import { useState } from "react";
+import useSWR from "swr";
 import styled from "styled-components";
 
-import Form from "../../components/ContactForm";
-
-const SearchContainer = styled.article`
+import SearchForm from "../../components/SearchForm";
 
 
-`
-
+const SearchContainer = styled.article``;
 
 export default function Search() {
-  const [query, setQuery] = useState('');
+  const [searchLocation, setSearchLocation] = useState("")
   const router = useRouter();
   const { isReady } = router;
   const { data, isLoading, error } = useSWR("/api/users", { fallbackData: [] });
-  
-  console.log("Data from Search: ", data);
-  console.log("Query from Search: ", query);
-  
-  
-
   if (!isReady || isLoading || error) return <h2>Loading...</h2>;
-  // Search should include: 
+   // Search should include: 
   // Input for Availability (date/calendar)
-  // Input for Location
   // Input for Price (Slider)
-  // Search Button 
-  // Should redirect the user to /results 
 
   // ?? next.js search using query string
   // in /results the user will see only the filtered items
   // https://medium.com/@ryanmambou/search-filter-in-nextjs-b30381c96f06
   // https://betterprogramming.pub/creating-a-multi-filter-function-to-filter-out-multiple-attributes-javascript-react-rails-5aad8e272142
 
-  function searchFilter(userData: any) {
-    const filterByLocation = userData.filter((element: any) => {
-      element.location.toLowerCase().includes(query)
-    })
-
+  function searchFilter(userData: any) {    
+    const filterByLocation = userData.filter((element: any) => element.location.toLowerCase().includes(searchLocation.toLowerCase()))    
+    return filterByLocation
   }
 
+  const results = searchFilter(data)
+  console.log(results);
+  
+
+  function handleSearch(element: any) {
+    setSearchLocation(element.target.location.value)
+
+  }
 
   return (
     <SearchContainer>
       <h1>Search!!</h1>
-      <Form></Form>
+      <SearchForm
+        onSubmit={handleSearch}
+        formName={"SearchForm"}
+        // onClick={() => router.push("/results")}
+      ></SearchForm>
     </SearchContainer>
-  )
-
+  );
 }
