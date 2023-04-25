0a3830c7162177ca135938b3b09552536c2947c4
christoph.raddatz <hello@chrstphrddtz.com>
2023-04-25 18:01:35 +0200

    Add link to search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
 import Head from 'next/head'
+import styled from 'styled-components'
 import { Figtree } from 'next/font/google'
+import Link from 'next/link'
 
 const figtree = Figtree({ subsets: ['latin'] })
+
+const MainContainer = styled.div`
+  height: 95vh;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+  gap: 0.5rem;
+  /* grid-template-columns: 2fr 3fr; */
+  @media (max-width: 844px) {
+    display: flex;
+    flex-direction: column;
+    gap: 0;
+  }
+`;
+
+const StyledLink = styled.a`
+  &:hover {
+    text-decoration: underline;
+  }
+`
 
 export default function Home() {
   return (
@@ -12,9 +35,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
+      <MainContainer>
        <h1>Welcome to BOXI ...</h1>
-      </div>
+       <h2><StyledLink href={"/search"} >Go to Search!!</StyledLink></h2>
+      </MainContainer>
     </>
   )
 }
