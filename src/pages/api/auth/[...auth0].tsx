8f8d984f555d30d74aab67483d5a7ed891a68e0b
christoph.raddatz <hello@chrstphrddtz.com>
2023-05-11 12:42:02 +0200

    add withPageAuthRequired and redirct after signup

--- src/pages/api/auth/[...auth0].tsx
+++ src/pages/api/auth/[...auth0].tsx
@@ -1,4 +1,7 @@
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 export default handleAuth({
-  signup: handleLogin({ authorizationParams: { screen_hint: 'signup' } })
-  });
+  signup: handleLogin({
+    authorizationParams: { screen_hint: "signup" },
+    returnTo: "/profile"
+  }),
+});
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -3,7 +3,8 @@ import { useRouter } from "next/router";
 import useSWR from "swr";
 import styled from "styled-components";
 
-import { useUser } from "@auth0/nextjs-auth0/client";
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0/client";
+
 import ProfileView from "../../../components/ProfileView";
 
 const RedirectDiv = styled.div`
@@ -19,34 +20,32 @@ const RedirectTitle = styled.h2`
   margin-top: 3rem;
 `;
 
-export default function Profile() {
+export default withPageAuthRequired(function Profile() {
   const { data } = useSWR("/api/users", { fallbackData: [] });
-  const router = useRouter();
-  const { push } = router;
-  const [redirectSeconds, setRedirectSeconds] = useState<number>(5);
-
   const { user, error, isLoading } = useUser();
+  // const router = useRouter();
+  // const { push } = router;
+  // const [redirectSeconds, setRedirectSeconds] = useState<number>(5);
 
-  useEffect(() => {
-    if (user === undefined) {
-      if (redirectSeconds === 0) {
-        push("/api/auth/signup");
-        return;
-      }
-      setTimeout(() => {
-        console.log(redirectSeconds);
-        setRedirectSeconds((redirectSeconds) => redirectSeconds - 1);
-      }, 1000);
-    }
-  }, [user, push, redirectSeconds]);
+  // useEffect(() => {
+  //   if (user === undefined) {
+  //     if (redirectSeconds === 0) {
+  //       push("/api/auth/signup");
+  //       return;
+  //     }
+  //     setTimeout(() => {
+  //       console.log(redirectSeconds);
+  //       setRedirectSeconds((redirectSeconds) => redirectSeconds - 1);
+  //     }, 1000);
+  //   }
+  // }, [user, push, redirectSeconds]);
 
   if (isLoading) return <div>Getting User Data...</div>;
   if (user === undefined) {
     return (
       <RedirectDiv>
         <RedirectTitle>
-          You don&apos;t have a profile yet. You will be redirected to the
-          Signup page in {redirectSeconds} seconds ...
+          You don&apos;t have a profile yet.
         </RedirectTitle>
       </RedirectDiv>
     );
@@ -60,4 +59,4 @@ export default function Profile() {
   // console.log("User from DB: ", findUser);
 
   return user && <ProfileView user={findUser} />;
-}
+});
