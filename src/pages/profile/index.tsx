47b1ed32f1b38895e2984b3b87fe4085c503ec0d
christoph.raddatz <hello@chrstphrddtz.com>
2023-05-19 12:00:00 +0200

    remove comments

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -23,30 +23,12 @@ const RedirectTitle = styled.h2`
 export default withPageAuthRequired(function Profile() {
   const { data } = useSWR("/api/users", { fallbackData: [] });
   const { user, error, isLoading } = useUser();
-  // const router = useRouter();
-  // const { push } = router;
-  // const [redirectSeconds, setRedirectSeconds] = useState<number>(5);
-
-  // useEffect(() => {
-  //   if (user === undefined) {
-  //     if (redirectSeconds === 0) {
-  //       push("/api/auth/signup");
-  //       return;
-  //     }
-  //     setTimeout(() => {
-  //       console.log(redirectSeconds);
-  //       setRedirectSeconds((redirectSeconds) => redirectSeconds - 1);
-  //     }, 1000);
-  //   }
-  // }, [user, push, redirectSeconds]);
 
   if (isLoading) return <div>Getting User Data...</div>;
   if (user === undefined) {
     return (
       <RedirectDiv>
-        <RedirectTitle>
-          You don&apos;t have a profile yet.
-        </RedirectTitle>
+        <RedirectTitle>You don&apos;t have a profile yet.</RedirectTitle>
       </RedirectDiv>
     );
   }
