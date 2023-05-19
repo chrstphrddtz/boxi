06c9a750609b8553a0e110a4d0e65a4c1dc002d7
christoph.raddatz <hello@chrstphrddtz.com>
2023-05-19 11:59:37 +0200

    Change Profile Edit route

--- src/pages/users/[id]/edit.tsx
+++ src/pages/users/[id]/edit.tsx
@@ -28,53 +28,45 @@ const H2 = styled.h2`
 `;
 
 export default function EditProfile() {
-  const router = useRouter();
-  const { isReady, push } = router;
-  const { id } = router.query;
-  const { data: user, isLoading, error } = useSWR(`/api/users/${id}`);
-
-  async function editUser(url: any, { arg }: any) {
-    const response = await fetch(url, {
-      method: "PUT",
-      body: JSON.stringify(arg),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      await response.json();
-    } else {
-      console.error(`Error: ${response.status}`);
-    }
-  }
-
-  const { trigger, isMutating } = useSWRMutation(`/api/users/${id}`, editUser);
-
-  async function handleEditUser(event: any) {
-    const formData = new FormData(event.target);
-    const userData = Object.fromEntries(formData);
-    const active = event.target.active.checked;
-    const newUserData = { ...userData, active };
-
-    await trigger(newUserData as any);
-    push("/profile");
-  }
-
-  if (!isReady || isLoading || error) return <h2>Loading...</h2>;
-  if (isMutating) {
-    return <h1>Submitting your changes</h1>;
-  }
-
-  return (
-    <Article>
-      <H2>Edit Profile</H2>
-
-      <EditProfileForm
-        onSubmit={handleEditUser}
-        formName={"edit-profile"}
-        defaultData={user}
-      />
-    </Article>
-  );
+  // const router = useRouter();
+  // const { isReady, push } = router;
+  // const { id } = router.query;
+  // const { data: user, isLoading, error } = useSWR(`/api/users/${id}`);
+  // async function editUser(url: any, { arg }: any) {
+  //   const response = await fetch(url, {
+  //     method: "PUT",
+  //     body: JSON.stringify(arg),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  //   if (response.ok) {
+  //     await response.json();
+  //   } else {
+  //     console.error(`Error: ${response.status}`);
+  //   }
+  // }
+  // const { trigger, isMutating } = useSWRMutation(`/api/users/${id}`, editUser);
+  // async function handleEditUser(event: any) {
+  //   const formData = new FormData(event.target);
+  //   const userData = Object.fromEntries(formData);
+  //   const active = event.target.active.checked;
+  //   const newUserData = { ...userData, active };
+  //   await trigger(newUserData as any);
+  //   push("/profile");
+  // }
+  // if (!isReady || isLoading || error) return <h2>Loading...</h2>;
+  // if (isMutating) {
+  //   return <h1>Submitting your changes</h1>;
+  // }
+  // return (
+  //   <Article>
+  //     <H2>Edit Profile</H2>
+  //     <EditProfileForm
+  //       onSubmit={handleEditUser}
+  //       formName={"edit-profile"}
+  //       defaultData={user}
+  //     />
+  //   </Article>
+  // );
 }
